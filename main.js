a9cd20c5b1c05dc1a7ec2f8859ee8fbb166a9b7c
Thuccy Nguyen <thuccynguyen@gmail.com>
2021-09-26 01:16:14 -0400

    implimented mapping and redirect functionality for long and short urls

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send({ 'Hello': 'World' })
-})
+// array of objects of format [{long: x, short: y}]
+let mappings = [];
 
-app.get('/:lurl', (req, res) => {
-    res.send({
-        'surl':(Math.random() + 1).toString(36).substring(7),
-        'q': req.query.q === undefined ? null : req.query.q,
-    
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// creates short token and appends object to mappings array
+app.post("/shorten/:lurl", async (req, res) => {
+  const lurl = req.params.lurl;
+  const surl = (Math.random() + 1).toString(36).substring(7);
+
+  if (!mappings.find(({ long }) => long === lurl)) {
+    //   if(!validUrl.isUri(lurl)) {
+    //       return res.status(401).json('Invalid input URL')
+    //   }
+    mappings.push({ long: lurl, short: surl });
+  }
+
+  let index = mappings
+    .map(function (e) {
+      return e.long;
     })
-})
+    .indexOf(lurl);
+  res.status(200).json({ index: index });
+});
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// returns the short value for indexed long url
+app.get("/results/:id", (req, res) => {
+  const index = req.params.id;
 
+  res.send(mappings[index].short);
+});
+
+// redirects to webpage given a short url
+app.get("/api/:surl", async (req, res) => {
+  const surl = req.params.surl;
+
+  let data = mappings.find((obj) => obj.short === surl);
+  res.redirect("https://" + data.long);
+});
+
+// returns the JSON object mapping given a long or short url
+app.get("/mapping/:url", (req, res) => {
+  const url = req.params.url;
+  res.json(mappings.find((obj) => obj.long === url || obj.short === url));
+});
+
+// returns all mapped elements
+app.get("/mappings", (req, res) => {
+  res.send(mappings);
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
